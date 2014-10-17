fa2816334480c10a88a9b215453c09c0a5f18aec
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-10-17 17:25:19 +0200

    add screens

--- core/src/ch/epfl/sweng/androfoot/AndroGame.java
+++ core/src/ch/epfl/sweng/androfoot/AndroGame.java
@@ -1,18 +1,30 @@
 package ch.epfl.sweng.androfoot;
 
+import ch.epfl.sweng.androfoot.screens.ConfigScreen;
+import ch.epfl.sweng.androfoot.screens.GameScreen;
+import ch.epfl.sweng.androfoot.screens.MenuScreen;
+import ch.epfl.sweng.androfoot.screens.SplashScreen;
+
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class AndroGame extends ApplicationAdapter {
+public class AndroGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
+	SplashScreen splashScreen;
+	GameScreen gameScreen;
+	MenuScreen menuScreen;
+	ConfigScreen configScreen;
+	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+		splashScreen = new SplashScreen();
+		setScreen(splashScreen);
 		img = new Texture("badlogic.jpg");
 	}
 
---
+++ core/src/ch/epfl/sweng/androfoot/screens/SplashScreen.java
@@ -0,0 +1,54 @@
+package ch.epfl.sweng.androfoot.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Display during loading
+ * @author Guillame Leclerc
+ *
+ */
+public class SplashScreen implements Screen {
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
