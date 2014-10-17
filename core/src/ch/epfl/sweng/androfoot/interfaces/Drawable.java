542f0d5eeb2fe42d5c7753dafc1d4b87ed23cc11
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-10-17 18:54:30 +0200

    Correction in Drawable interface (Inheritance forgotten)

--- core/src/ch/epfl/sweng/androfoot/interfaces/Drawable.java
+++ core/src/ch/epfl/sweng/androfoot/interfaces/Drawable.java
@@ -5,7 +5,7 @@ package ch.epfl.sweng.androfoot.interfaces;
  * @author Guillame Leclerc
  *
  */
-public interface Drawable {
+public interface Drawable extends Visitable{
 	/**
 	 * Specify the z-index of a Drawable object (Objects are drawn in ascending order)
 	 * @return an integer that represent the the priority given to an object.
