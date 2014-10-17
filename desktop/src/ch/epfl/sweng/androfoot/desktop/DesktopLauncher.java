eca32d5b9c26601ceffe658918847eee887f739e
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-10-17 09:43:40 +0200

    project stub

---
+++ desktop/src/ch/epfl/sweng/androfoot/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ch.epfl.sweng.androfoot.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ch.epfl.sweng.androfoot.AndroGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AndroGame(), config);
+	}
+}
