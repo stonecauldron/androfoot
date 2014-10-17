04b47193d3280118641b43ec76c4479948ab0939
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-10-17 17:29:08 +0200

    Add interfaces for visitor pattern

---
+++ core/src/ch/epfl/sweng/androfoot/interfaces/Visitable.java
@@ -0,0 +1,5 @@
+package ch.epfl.sweng.androfoot.interfaces;
+
+public interface Visitable {
+	public void accept(Visitor visitor);
+}
---
+++ core/src/ch/epfl/sweng/androfoot/interfaces/Visitor.java
@@ -0,0 +1,7 @@
+package ch.epfl.sweng.androfoot.interfaces;
+
+public interface Visitor {
+	
+	public void visit(Visitable visitable);
+
+}
