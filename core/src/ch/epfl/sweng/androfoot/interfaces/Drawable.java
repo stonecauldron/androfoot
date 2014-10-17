b9a9ff6f97d20cdfa888c5e7acc408104c0d242b
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-10-17 18:05:02 +0200

    new Interface : Drawable

---
+++ core/src/ch/epfl/sweng/androfoot/interfaces/Drawable.java
@@ -0,0 +1,14 @@
+package ch.epfl.sweng.androfoot.interfaces;
+
+/**
+ * Represents an object that can be drow by a worldRenderer
+ * @author Guillame Leclerc
+ *
+ */
+public interface Drawable {
+	/**
+	 * Specify the z-index of a Drawable object (Objects are drawn in ascending order)
+	 * @return an integer that represent the the priority given to an object.
+	 */
+	public int getZIndex();
+}
