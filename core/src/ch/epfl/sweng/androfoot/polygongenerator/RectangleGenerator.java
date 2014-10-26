4a936a3007687a06f363950edaad5d3813e02097
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-10-26 14:47:51 +0100

    add a Rectangle generator

---
+++ core/src/ch/epfl/sweng/androfoot/polygongenerator/RectangleGenerator.java
@@ -0,0 +1,42 @@
+package ch.epfl.sweng.androfoot.polygongenerator;
+
+import java.util.List;
+
+import com.badlogic.gdx.math.Rectangle;
+
+import ch.epfl.sweng.androfoot.interfaces.PolygonGenerator;
+
+public class RectangleGenerator extends AbstractPolygonGenerator {
+	
+	private final static int NB_VERTEX_COORDS_RECTANGLE = 8;
+	
+	private final Rectangle rectangle;
+	private final float[] vertexes;
+	private boolean isGenerated;
+		
+	
+	public RectangleGenerator(Rectangle rectangleArg) {
+		rectangle = new Rectangle(rectangleArg);
+		isGenerated = false;
+		vertexes = new float[NB_VERTEX_COORDS_RECTANGLE];
+	}
+
+
+	@Override
+	protected float[] generate() {
+		if(isGenerated) {
+			return vertexes;
+		}
+		vertexes[0] = rectangle.x;
+		vertexes[1] = rectangle.y;
+		vertexes[2] = vertexes[0]+ rectangle.width;
+		vertexes[3] = vertexes[1];
+		vertexes[4] = vertexes[2];
+		vertexes[5] = vertexes[3] - rectangle.height;
+		vertexes[6] = vertexes[0];
+		vertexes[7] = vertexes[5];
+		
+		isGenerated = true;
+		return vertexes;
+	}
+}
