5bd4bc26b33bf4d6e658b479649601ea70605760
Adan Häfliger <adan.haefliger@epfl.ch>
2014-10-28 13:16:15 +0100

    Add speed ratio scaling (refining)

--- core/src/ch/epfl/sweng/androfoot/screens/GameScreen.java
+++ core/src/ch/epfl/sweng/androfoot/screens/GameScreen.java
@@ -11,42 +11,47 @@ import com.badlogic.gdx.Screen;
 
 /**
  * Display the game
+ * 
  * @author Guillame Leclerc
  *
  */
 public class GameScreen implements Screen, TouchTrackerObserver {
 
-	private GroupPaddle mPaddlesOnePlayerOne = new GroupPaddle(2, 2, 1, PhysicsWorld.getPhysicsWorld().getWorld(), Constants.WORLD_SIZE_Y, true);
+	private final static float X_SPEED_RATIO = (float) 0.75;
+	private final static float Y_SPEED_RATIO = (float) 0.75;
+
+	private GroupPaddle mPaddlesOnePlayerOne = new GroupPaddle(2, 2, 1,
+			PhysicsWorld.getPhysicsWorld().getWorld(), Constants.WORLD_SIZE_Y,
+			true);
 
 	private float mPlayerOneOldX = 0;
 	private float mPlayerOneOldY = 0;
 	private boolean mPlayerOneOldTouched = false;
-	
-	
+
 	@Override
 	public void render(float delta) {
 		PhysicsWorld.getPhysicsWorld().phyStep(delta);
 		GraphicEngine.getEngine().render();
 	}
 
-	
 	@Override
 	public void update(int playerId, float posX, float posY, boolean touched) {
 		if (mPlayerOneOldTouched == true && touched == true) {
-			mPaddlesOnePlayerOne.setVelocity(posX - mPlayerOneOldX, mPlayerOneOldY - posY);
+			mPaddlesOnePlayerOne.setVelocity((posX - mPlayerOneOldX) * X_SPEED_RATIO,
+					(mPlayerOneOldY - posY) * Y_SPEED_RATIO);
 			mPlayerOneOldX = posX;
 			mPlayerOneOldY = posY;
 			mPlayerOneOldTouched = touched;
-		} else if (touched){
+		} else if (touched) {
 			mPlayerOneOldTouched = touched;
 			mPlayerOneOldX = posX;
 			mPlayerOneOldY = posY;
 		} else {
-			mPaddlesOnePlayerOne.setVelocity(0,0);
+			mPaddlesOnePlayerOne.setVelocity(0, 0);
 			mPlayerOneOldTouched = false;
 		}
 	}
-	
+
 	@Override
 	public void resize(int width, int height) {
 		GraphicEngine.getEngine().setScreenSize(width, height);
@@ -55,7 +60,7 @@ public class GameScreen implements Screen, TouchTrackerObserver {
 	@Override
 	public void show() {
 		GraphicEngine.getEngine().bindToWorld(PhysicsWorld.getPhysicsWorld());
-	
+
 		SinglePlayerTouchTracker sptt = SinglePlayerTouchTracker.getInstance();
 		sptt.addObserver(this);
 	}
