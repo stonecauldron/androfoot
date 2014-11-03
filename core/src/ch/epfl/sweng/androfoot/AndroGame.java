354f3229c2b23271dbb913992fc0341cea1e2840
Gaylor Bosson <gaylor.bosson@gmail.com>
2014-11-03 11:09:07 +0100

    Fix the breaked code. The game start in gameScreen waiting for the fix for the menuScreen

--- core/src/ch/epfl/sweng/androfoot/AndroGame.java
+++ core/src/ch/epfl/sweng/androfoot/AndroGame.java
@@ -1,24 +1,17 @@
 package ch.epfl.sweng.androfoot;
 
-import ch.epfl.sweng.androfoot.box2dphysics.Constants;
-import ch.epfl.sweng.androfoot.box2dphysics.GroupPaddle;
-import ch.epfl.sweng.androfoot.box2dphysics.PhysicsWorld;
-import ch.epfl.sweng.androfoot.screens.ConfigScreen;
 import ch.epfl.sweng.androfoot.screens.GameScreen;
-import ch.epfl.sweng.androfoot.screens.MainMenuScreen;
-import ch.epfl.sweng.androfoot.screens.SplashScreen;
-
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class AndroGame extends Game {
 	SpriteBatch batch;
 	
-	MainMenuScreen mainMenuScreen;
+	GameScreen gameScreen;
 	
 	@Override
 	public void create () {
-		mainMenuScreen = new MainMenuScreen();
-		setScreen(mainMenuScreen);
+		gameScreen = new GameScreen();
+		setScreen(gameScreen);
 	}
 }
