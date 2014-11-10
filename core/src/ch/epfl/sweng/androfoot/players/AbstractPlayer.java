8c028284d8e499b1994fccb9f8f5e8c26703b13d
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2014-11-10 11:16:15 +0100

    AbstractPlayer initialises paddle group layout

--- core/src/ch/epfl/sweng/androfoot/players/AbstractPlayer.java
+++ core/src/ch/epfl/sweng/androfoot/players/AbstractPlayer.java
@@ -2,6 +2,7 @@ package ch.epfl.sweng.androfoot.players;
 
 import java.util.List;
 
+import ch.epfl.sweng.androfoot.box2dphysics.Constants;
 import ch.epfl.sweng.androfoot.box2dphysics.GroupPaddle;
 
 /**
@@ -11,12 +12,22 @@ import ch.epfl.sweng.androfoot.box2dphysics.GroupPaddle;
  */
 public abstract class AbstractPlayer {
 	
+	// offset between the paddles
+	private static final float PADDLE_OFFSET = Constants.WORLD_SIZE_X/5.0f;
+	// number of defensors
+	private static final int DEFENSOR_NUMBER = 3;
+	// number of attackers
+	private static final int ATTACKER_NUMBER = 2;
+	
+	// all the paddles belonging to a player
 	private List<GroupPaddle> paddles;
 	
 	private PlayerNumber playerNumber;
 	
 	public AbstractPlayer(PlayerNumber number) {
 		playerNumber = number;
+		
+		initialisePaddleLayout();
 	}
 	
 	protected List<GroupPaddle> getPaddles() {
@@ -25,5 +36,29 @@ public abstract class AbstractPlayer {
 	
 	protected PlayerNumber getPlayerNumber() {
 		return playerNumber;
+	}
+	
+	private void initialisePaddleLayout() {
+		// TODO create createPaddleGroup method in the physics Engine
+		// will not have references to physics world and physics world height
+		// createPaddleGroupOffense(x coordinate)
+		// createPaddleGroupdDefense(x coordinate)
+		
+		// TODO BoardFactory
+		// Creates a board object and defines wich actual player instances will
+		// be created.
+		float defenseXCoordinate = 0f;
+		float attackXCoordinate = 0f;
+		
+		if (playerNumber == PlayerNumber.ONE) {
+			defenseXCoordinate = PADDLE_OFFSET;
+			attackXCoordinate = PADDLE_OFFSET * 3;
+		}
+		else if (playerNumber == PlayerNumber.TWO) {
+			defenseXCoordinate = PADDLE_OFFSET * 4;
+			attackXCoordinate = PADDLE_OFFSET * 2;
+		}
+		GroupPaddle.createGroupPaddle(defenseXCoordinate, DEFENSOR_NUMBER, playerNumber);
+		GroupPaddle.createGroupPaddle(attackXCoordinate, ATTACKER_NUMBER, playerNumber);
 	}
 }
