1ffce80e56ce15cffc63b1026d2423737fcc9f0f
Adan Häfliger <adan.haefliger@epfl.ch>
2014-11-16 22:40:15 +0100

    Refine sound (fixes #39)

--- core/src/ch/epfl/sweng/androfoot/soundeffect/SoundEffectManager.java
+++ core/src/ch/epfl/sweng/androfoot/soundeffect/SoundEffectManager.java
@@ -8,7 +8,6 @@ import ch.epfl.sweng.androfoot.interfaces.BorderObserver;
 import ch.epfl.sweng.androfoot.interfaces.GoalObserver;
 import ch.epfl.sweng.androfoot.interfaces.PaddleContactObserver;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Sound;
 
@@ -18,7 +17,8 @@ import com.badlogic.gdx.audio.Sound;
  *         This class manages sound effects throughout the game listening to the
  *         physics engine's events
  */
-public enum SoundEffectManager implements GoalObserver, PaddleContactObserver, BorderObserver {
+public enum SoundEffectManager implements GoalObserver, PaddleContactObserver,
+		BorderObserver {
 	INSTANCE;
 
 	private Sound mBallOnWall;
@@ -29,15 +29,18 @@ public enum SoundEffectManager implements GoalObserver, PaddleContactObserver, B
 	private String mBallGoalPlayerOne = "sounds/goalplayerone.wav";
 	private String mBallOnPaddlePath = "sounds/ballpaddle.wav";
 
-	SoundEffectManager() {
-	}
-	
+	/**
+	 * Used to listen to the various game events
+	 */
 	public void observe() {
 		EventManager.getEventManager().addGoalObserver(this);
 		EventManager.getEventManager().addPaddleContactObserver(this);
 		EventManager.getEventManager().addBorderContactObserver(this);
 	}
 
+	/**
+	 * Call me to load sound assets
+	 */
 	public void loadSoundEffect() {
 		AssetManager manager = new AssetManager();
 		manager.load(mBallOnWallPath, Sound.class);
@@ -47,31 +50,36 @@ public enum SoundEffectManager implements GoalObserver, PaddleContactObserver, B
 		mBallOnWall = manager.get(mBallOnWallPath, Sound.class);
 		mGoalPlayerOne = manager.get(mBallGoalPlayerOne, Sound.class);
 		mBallOnPaddle = manager.get(mBallOnPaddlePath, Sound.class);
-		
+
 	}
 
+	/**
+	 * 
+	 * @return the unique instance of the SoundEffectManager
+	 */
 	public static SoundEffectManager getInstance() {
 		return INSTANCE;
 	}
 
-	private float pitchRandomizer() {
+	private float pitchRandomizer(int offSet) {
 		Random rand = new Random();
-		int randomNum = rand.nextInt((250 - 50) + 1) + 50;
+		int randomNum = rand.nextInt((250 - offSet) + 1) + offSet;
+
 		return (float) (randomNum / 100);
 	}
 
 	@Override
 	public void goal(boolean isTeamOne) {
-		mGoalPlayerOne.play();
+		mGoalPlayerOne.play((float) 0.3, 1, 0);
 	}
 
 	@Override
 	public void borderContact(BorderType type) {
-		//mBallOnWall.play();
+		mBallOnWall.play((float) 0.1, 1, 0);
 	}
 
 	@Override
 	public void paddleContact() {
-		mBallOnPaddle.play();
+		mBallOnPaddle.play(1, pitchRandomizer(125), 0);
 	}
 }
