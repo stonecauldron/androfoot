ad86781bb7901f6a5e7e7218accd26ca711c6ac8
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2014-11-16 17:08:14 +0100

    AIPlayer now observes the AIEngine upon creation

--- core/src/ch/epfl/sweng/androfoot/players/AIPlayer.java
+++ core/src/ch/epfl/sweng/androfoot/players/AIPlayer.java
@@ -1,22 +1,28 @@
 package ch.epfl.sweng.androfoot.players;
 
 import ch.epfl.sweng.androfoot.interfaces.Controllable;
+import ch.epfl.sweng.androfoot.players.ai.AIEngine;
+import ch.epfl.sweng.androfoot.players.ai.AIObserver;
 
 /**
  * Class to represent an AI controlled player.
+ * 
  * @author Pedro Caldeira <pedrocaldeira>
  *
  */
-public class AIPlayer extends AbstractPlayer implements Controllable {
+public class AIPlayer extends AbstractPlayer implements Controllable,
+		AIObserver {
 
 	AIPlayer(PlayerNumber number) {
 		super(number);
+
+		// start observing AIEngine
+		AIEngine.getInstance().addAIObserver(this);
 	}
 
 	@Override
 	public void moveHorizontally(float deltaX) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -27,7 +33,11 @@ public class AIPlayer extends AbstractPlayer implements Controllable {
 	@Override
 	public void move(float deltaX, float deltaY) {
 		// TODO Auto-generated method stub
-		
 	}
-	
+
+	@Override
+	public void update(float deltaTime) {
+		// TODO Auto-generated method stub
+	}
+
 }
