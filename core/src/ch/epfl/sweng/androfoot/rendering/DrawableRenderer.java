0c1041aee9283ff7aefc960d5317abba227b5278
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-11-17 12:14:50 +0100

    add doc for DrawableRenderer

--- core/src/ch/epfl/sweng/androfoot/rendering/DrawableRenderer.java
+++ core/src/ch/epfl/sweng/androfoot/rendering/DrawableRenderer.java
@@ -3,7 +3,17 @@ package ch.epfl.sweng.androfoot.rendering;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+/**
+ * Can be rendered by the graphic engine
+ * @author Guillame Leclerc
+ *
+ */
 public interface DrawableRenderer {
 	
+	/**
+	 * Render this object
+	 * @param batch the sprite batch
+	 * @param shapes the shape renderer
+	 */
 	public void render(SpriteBatch batch, ShapeRenderer shapes) ;
 }
