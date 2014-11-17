341f22c53489c159d9deb221597a55894e0ca4e5
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-11-17 12:45:01 +0100

    add doc for polygonRenderer

--- core/src/ch/epfl/sweng/androfoot/rendering/PolygonRenderer.java
+++ core/src/ch/epfl/sweng/androfoot/rendering/PolygonRenderer.java
@@ -2,8 +2,17 @@ package ch.epfl.sweng.androfoot.rendering;
 
 import ch.epfl.sweng.androfoot.interfaces.PolygonGenerator;
 
+/**
+ * A class able to render a Polygon 
+ * @author Guillame Leclerc
+ *
+ */
 public class PolygonRenderer extends AbstractMeshRenderer {
 	
+	/**
+	 * Build the renderer from a polygon generator
+	 * @param generator
+	 */
 	public PolygonRenderer(PolygonGenerator generator) {
 		super(new PolygonToMesh(generator).build());
 	}
