b8da5d5ab5a4075a3cdd5036f0cfb757811c2ba3
MatveyK <matvey.khokhlov@gmail.com>
2014-11-24 12:57:04 +0100

    Modified PlayerContactListener to be more coherent with other listeners

--- desktop/test/test/ch/epfl/sweng/androfoot/box2dphysics/PlayerContactObserverTest.java
+++ desktop/test/test/ch/epfl/sweng/androfoot/box2dphysics/PlayerContactObserverTest.java
@@ -1,47 +1,64 @@
 package test.ch.epfl.sweng.androfoot.box2dphysics;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 
 import ch.epfl.sweng.androfoot.box2dphysics.Ball;
 import ch.epfl.sweng.androfoot.box2dphysics.Constants;
-import ch.epfl.sweng.androfoot.box2dphysics.EventManager;
 import ch.epfl.sweng.androfoot.box2dphysics.GroupPaddle;
-import ch.epfl.sweng.androfoot.box2dphysics.PhysicsWorld;
 import ch.epfl.sweng.androfoot.box2dphysics.Player;
+import ch.epfl.sweng.androfoot.box2dphysics.PlayerContactListener;
 import ch.epfl.sweng.androfoot.interfaces.PlayerObserver;
 import junit.framework.TestCase;
 
 public class PlayerContactObserverTest extends TestCase implements
 		PlayerObserver {
 
-	private PhysicsWorld world = PhysicsWorld.getPhysicsWorld();
-	private Ball ball = PhysicsWorld.createBall(Constants.BALL_INIT_POS_X,
-			Constants.BALL_INIT_POS_Y, Constants.BALL_RADIUS);
-	
+	private World world = new World(new Vector2(0, 0), false);
+
 	private boolean eventOcurred = false;
-	
+
+	private Ball ball = new Ball(world, Constants.BALL_INIT_POS_X,
+			Constants.BALL_INIT_POS_Y, Constants.BALL_RADIUS,
+			Constants.BALL_DENSITY, Constants.BALL_FRICTION,
+			Constants.BALL_RESTITUTION);
+
+	@Before
+	public void initilalize() {
+		world.setContactListener(PlayerContactListener.getInstance());
+		PlayerContactListener.setEventManager(EventManagerTester
+				.getEventManager());
+	}
+
 	@Test
 	public void testIfPlayerEventOcurredOnFront() {
-		EventManager.getEventManager().addPlayerObserver(this);
+		EventManagerTester.getEventManager().addPlayerObserver(this);
 		
-		GroupPaddle paddle = PhysicsWorld.createPaddle(1.0f, Constants.PADDLE_WIDTH, 1, true);
+		GroupPaddle paddleFacingRight = new GroupPaddle(world, 1.0f,
+				Constants.PADDLE_WIDTH, 1, true);
+
 		ball.setLinearVelocity(-Constants.BALL_MAX_VELOCITY, 0f);
-		
-		multipleStep(50);
-		
+
+		multipleStep(200);
+
 		assertFalse(eventOcurred);
 	}
-	
+
 	@Test
 	public void testIfPlayerEventOcurredOnBack() {
-		EventManager.getEventManager().addPlayerObserver(this);
+		EventManagerTester.getEventManager().addPlayerObserver(this);
+
+		GroupPaddle paddleFacingLeft = new GroupPaddle(world, 1.0f, Constants.PADDLE_WIDTH, 3, false);
 		
-		PhysicsWorld.createPaddle(3.0f, Constants.PADDLE_WIDTH, 1, false);
-		ball.setBallPosition(Constants.BALL_INIT_POS_X * 1.5f, Constants.BALL_INIT_POS_Y);
+		ball.setBallPosition(Constants.BALL_INIT_POS_X * 1.5f,
+				Constants.BALL_INIT_POS_Y);
 		ball.setLinearVelocity(-Constants.BALL_MAX_VELOCITY, 0f);
-		
-		multipleStep(50);
-		
+
+		multipleStep(200);
+
 		assertTrue(eventOcurred);
 	}
 
@@ -52,9 +69,10 @@ public class PlayerContactObserverTest extends TestCase implements
 
 	private void multipleStep(int nbSteps) {
 		for (int i = 0; i < nbSteps; i++) {
-			world.getBox2DWorld().step(1, Constants.VELOCITY_ITERATIONS, Constants.POSITION_ITERATIONS);
+			world.step(1 / 60.0f, Constants.VELOCITY_ITERATIONS,
+					Constants.POSITION_ITERATIONS);
 
-			EventManager.getEventManager().throwEvents();
+			EventManagerTester.getEventManager().throwEvents();
 		}
 	}
 }
