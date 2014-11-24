c30209739cc699d63d7bddccc85e5a1c254ae4d4
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-11-24 08:04:47 +0100

    add a new shader for collisions between borders and ball

---
+++ core/src/ch/epfl/sweng/androfoot/rendering/shaders/BoomShockwaveShader.java
@@ -0,0 +1,25 @@
+package ch.epfl.sweng.androfoot.rendering.shaders;
+
+public class BoomShockwaveShader extends ShockwaveShader {
+	
+	@Override
+	protected String getFragmentShader() {
+		return 
+				"#ifdef GL_ES\n" 
+                + "precision mediump float;\n"  
+                + "#endif\n" 
+				+ "varying vec4 v_originalPoint;"
+				+ "varying vec4 v_color;"
+				+ "uniform vec3 u_center;"
+				+ "uniform float u_radius;"
+				+ "void main() {"
+				+ "vec2 op = vec2(v_originalPoint.x, v_originalPoint.y);"
+				+ "vec2 ref = vec2(u_center.x, u_center.y);"
+				+ "float x = distance(ref, op);"
+				+ "vec4 resu = v_color;"
+				+ "float comp = clamp((-0.1 / (x-u_radius)),0.1,1.0);"
+				+ "resu.a -= 8.0/256.0;"
+				+ "gl_FragColor = resu;"
+				+ "}";
+	}
+}
