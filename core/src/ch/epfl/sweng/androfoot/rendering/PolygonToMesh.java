8b2210b10a31b46342022565c8ab263a9381e5cf
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-11-24 15:34:55 +0100

    add documentation for PolygonToMesh.java

--- core/src/ch/epfl/sweng/androfoot/rendering/PolygonToMesh.java
+++ core/src/ch/epfl/sweng/androfoot/rendering/PolygonToMesh.java
@@ -6,9 +6,18 @@ import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.VertexAttribute;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 
+/**
+ * Adapater class from a {@link PolygonGenerator} to {@link Mesh}
+ * @author Guillame Leclerc
+ *
+ */
 public class PolygonToMesh implements MeshBuilder {
 	private final Mesh mesh;
 	
+	/**
+	 * Build the adaptater from a {@link PolygonGenerator}
+	 * @param generator the generator to use
+	 */
 	public PolygonToMesh(PolygonGenerator generator) {
 		mesh = new Mesh(false, generator.generatePointsList().size(), 0, 
 				new VertexAttribute(Usage.Position,3, "a_position"));
