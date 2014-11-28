912184b3f7fabd2d49495738d266bbee4f06e44a
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-11-28 10:07:13 +0100

    Cache results for polygonGenerators

--- core/src/ch/epfl/sweng/androfoot/polygongenerator/AbstractPolygonGenerator.java
+++ core/src/ch/epfl/sweng/androfoot/polygongenerator/AbstractPolygonGenerator.java
@@ -6,10 +6,12 @@ import java.util.List;
 import ch.epfl.sweng.androfoot.interfaces.PolygonGenerator;
 
 /**
- * Provide some default implementations of the the interface so the different representations
- * of the polygon are consistent
+ * Provide some default implementations of the the interface so the different
+ * representations of the polygon are consistent
  * 
- * To use the features of this class you need to provide a method {@code generate()}
+ * To use the features of this class you need to provide a method
+ * {@code generate()}
+ * 
  * @author Guillame Leclerc
  * @see PolygonGenerator
  *
@@ -18,37 +20,45 @@ public abstract class AbstractPolygonGenerator implements PolygonGenerator {
 
 	/**
 	 * generate the polygon and return the vertexes
-	 * @return an array containing the vertexes in 2D (even indexes for X coords and odd for Y coord)
-	 * 		
+	 * 
+	 * @return an array containing the vertexes in 2D (even indexes for X coords
+	 *         and odd for Y coord)
+	 * 
 	 */
 	protected abstract float[] generate();
 
+	private float[] vertexes = null;
+
 	@Override
 	public float[] generateVertexesFloat() {
-		return generate();
+		if (vertexes == null) {
+			vertexes = generate();
+		}
+		return vertexes;
 	}
 
 	@Override
 	public float[] generateVertexesFloatInZPlane(float z) {
-		float[] vertexes = generate();
-		int segments = vertexes.length/2 - 1;
+		float[] vertexes = generateVertexesFloat();
+		int segments = vertexes.length / 2 - 1;
 		float[] vertexes3D = new float[(segments + 1) * 3];
-		for (int i = 0 ; i <= segments ; i++){
-			vertexes3D[3*i] = vertexes[2*i];
-			vertexes3D[3*i + 1] = vertexes[2*i + 1];
-			vertexes3D[3*i + 2] = z;
+		for (int i = 0; i <= segments; i++) {
+			vertexes3D[3 * i] = vertexes[2 * i];
+			vertexes3D[3 * i + 1] = vertexes[2 * i + 1];
+			vertexes3D[3 * i + 2] = z;
 		}
-		
+
 		return vertexes3D;
 	}
 
 	@Override
 	public List<ImmutablePoint<Float>> generatePointsList() {
-		float[] vertexes = generate();
-		int segments = vertexes.length/2 - 1;
+		float[] vertexes = generateVertexesFloat();
+		int segments = vertexes.length / 2 - 1;
 		ArrayList<ImmutablePoint<Float>> result = new ArrayList<ImmutablePoint<Float>>();
-		for (int i = 0 ; i <= segments ; i ++) {
-			result.add(new ImmutablePoint<Float>(vertexes[2*i],vertexes[2*i + 1]));
+		for (int i = 0; i <= segments; i++) {
+			result.add(new ImmutablePoint<Float>(vertexes[2 * i],
+					vertexes[2 * i + 1]));
 		}
 
 		return result;
