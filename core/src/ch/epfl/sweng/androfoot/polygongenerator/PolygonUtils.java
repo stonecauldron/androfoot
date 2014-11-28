2514e8c6990c49da6180643729a8999d34823b9d
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-11-28 10:13:31 +0100

    refactor satic methods to a Utilitary class

---
+++ core/src/ch/epfl/sweng/androfoot/polygongenerator/PolygonUtils.java
@@ -0,0 +1,36 @@
+package ch.epfl.sweng.androfoot.polygongenerator;
+
+/**
+ * Provide some static utilitary methods and usefull constants
+ * 
+ * @author Guillaume
+ *
+ */
+final class PolygonUtils {
+	static final float MIN_ANGLE = 0f;
+	static final float MAX_ANGLE_RADIAN = (float) Math.PI * 2;
+	static final float MAX_ANGLE_DEGREE = 360f;
+
+	/**
+	 * Converts an angle from degree to radians
+	 * 
+	 * @param angleDegree
+	 *            the angle in degree
+	 * @return the angle in radians
+	 */
+	public static float degreeToRadian(float angleDegree) {
+		return angleDegree / MAX_ANGLE_DEGREE * MAX_ANGLE_RADIAN;
+	}
+
+	/**
+	 * Simplify an angle in radians
+	 * 
+	 * @param angle
+	 *            a simplified angle (between 0 and 2Pi)
+	 * @return
+	 */
+	public static float simplifyRadianAngle(float angle) {
+		double divided = Math.floor(angle / MAX_ANGLE_RADIAN);
+		return (float) (angle - divided * MAX_ANGLE_RADIAN);
+	}
+}
