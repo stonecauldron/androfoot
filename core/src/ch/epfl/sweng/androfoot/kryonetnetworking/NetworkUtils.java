32deaa4973d62237b7300adea49e5d4833785879
Adan Häfliger <adan.haefliger@epfl.ch>
2014-11-28 08:02:14 +0100

    create networking utilites class

--- core/src/ch/epfl/sweng/androfoot/kryonetnetworking/NetworkUtils.java
+++ core/src/ch/epfl/sweng/androfoot/kryonetnetworking/NetworkUtils.java
@@ -6,12 +6,10 @@ import com.esotericsoftware.kryonet.EndPoint;
 public class NetworkUtils {
 	static public final int TCP_PORT = 40000;
 	static public final int UDP_PORT = 40005;
-	
 	// This registers objects that are going to be sent over the network.
     static public void register (EndPoint endPoint) {
             Kryo kryo = endPoint.getKryo();
-            kryo.register(HostData.class);
-            kryo.register(InputData.class);
+            kryo.register(GameData.class);
+            kryo.register(TouchData.class);
     }
-    
 }
