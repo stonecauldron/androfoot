66a25081d7a5e5388ee89d4187ec301e6ecb120b
Adan Häfliger <adan.haefliger@epfl.ch>
2014-11-28 17:11:15 +0100

    Add test to try connection

---
+++ core/src/ch/epfl/sweng/androfoot/kryonetnetworking/NetworkTest.java
@@ -0,0 +1,31 @@
+package ch.epfl.sweng.androfoot.kryonetnetworking;
+
+import static org.junit.Assert.fail;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class NetworkTest {
+
+	private static PlayerHost ph;
+	private static PlayerClient client;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+		System.setProperty("java.net.preferIPv4Stack", "true");
+		
+		ph = new PlayerHost();
+		ph.listenToClient();
+
+		client = new PlayerClient();
+		client.listenToServer();
+
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
