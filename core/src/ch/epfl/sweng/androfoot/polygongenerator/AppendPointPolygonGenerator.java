6d9b7be883ecf320d88f6724441738f8dbad76ff
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-12-01 20:14:06 +0100

    Add Doc and checkstyle compliance for AppendPointPolygonGenerator.java

--- core/src/ch/epfl/sweng/androfoot/polygongenerator/AppendPointPolygonGenerator.java
+++ core/src/ch/epfl/sweng/androfoot/polygongenerator/AppendPointPolygonGenerator.java
@@ -2,23 +2,34 @@ package ch.epfl.sweng.androfoot.polygongenerator;
 
 import ch.epfl.sweng.androfoot.interfaces.PolygonGenerator;
 
-public class AppendPointPolygonGenerator extends AbstractPolygonGenerator
-		implements PolygonGenerator {
+/**
+ * A class that wrapp a {@link PolygonGenerator} and append a point at the end
+ * of the polygon
+ * 
+ * @author Guillame Leclerc
+ *
+ */
+public class AppendPointPolygonGenerator extends AbstractPolygonGenerator implements PolygonGenerator {
 
-	ImmutablePoint<Float> point;
-	PolygonGenerator generator;
-	
+	private ImmutablePoint<Float> point;
+	private PolygonGenerator generator;
+
+	/**
+	 * Create a {@link AppendPointPolygonGenerator} from another {@link PolygonGenerator}
+	 * @param generatorArg the {@link PolygonGenerator}
+	 * @param pointArg the point to append at the end of the polygon
+	 */
 	public AppendPointPolygonGenerator(PolygonGenerator generatorArg, ImmutablePoint<Float> pointArg) {
 		point = pointArg;
-		generator=generatorArg;
+		generator = generatorArg;
 	}
-	
+
 	@Override
 	protected float[] generate() {
 		float[] originalVertexes = generator.generateVertexesFloat();
-		float[] vertexes = new float[originalVertexes.length+2];
-		vertexes[vertexes.length-2] = point.x;
-		vertexes[vertexes.length-1] = point.y;
+		float[] vertexes = new float[originalVertexes.length + 2];
+		vertexes[vertexes.length - 2] = point.x;
+		vertexes[vertexes.length - 1] = point.y;
 		System.arraycopy(originalVertexes, 0, vertexes, 0, originalVertexes.length);
 		return vertexes;
 	}
