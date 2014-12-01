6c316ddc71f085f1e90ad26754f1300e3e1a81b8
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2014-12-01 18:12:41 +0100

    Reduce scale of the game

--- core/src/ch/epfl/sweng/androfoot/gamelogic/PlayerCharacteristicsManager.java
+++ core/src/ch/epfl/sweng/androfoot/gamelogic/PlayerCharacteristicsManager.java
@@ -13,9 +13,9 @@ import ch.epfl.sweng.androfoot.polygongenerator.PaddleGenerator;
  */
 public class PlayerCharacteristicsManager {
 	
-	private static float DEFAULT_PADDLE_HEIGHT_CIRCLE = 0.25f;
-	private static float DEFAULT_PADDLE_HEIGHT_RECTANGLE = 0.1f;
-	private static float DEFAULT_PADDLE_WIDTH = 1f;
+	private static float DEFAULT_PADDLE_HEIGHT_CIRCLE = 0.2f;
+	private static float DEFAULT_PADDLE_HEIGHT_RECTANGLE = 0.08f;
+	private static float DEFAULT_PADDLE_WIDTH = 0.8f;
 	
 	private static PaddleGenerator paddleGeneratorInstanceTeam1 = null;
 	private static PaddleGenerator paddleGeneratorInstanceTeam2 = null;
