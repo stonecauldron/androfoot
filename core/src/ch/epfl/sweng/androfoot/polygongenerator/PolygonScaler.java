fd513b2bbfacd8346708c018900b6a6ee4c9fa12
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-12-01 20:31:42 +0100

    Add Doc and checkstyle compliance for PolygonScaler.java

--- core/src/ch/epfl/sweng/androfoot/polygongenerator/PolygonScaler.java
+++ core/src/ch/epfl/sweng/androfoot/polygongenerator/PolygonScaler.java
@@ -5,33 +5,40 @@ import java.util.Arrays;
 import ch.epfl.sweng.androfoot.interfaces.PolygonGenerator;
 
 /**
- * A scaled version of a {@link PolygonGenerator} 
+ * A scaled version of a {@link PolygonGenerator}
+ * 
  * @author Guillame Leclerc
  *
  */
-public class PolygonScaler extends AbstractPolygonGenerator implements
-		PolygonGenerator {
-	
+public class PolygonScaler extends AbstractPolygonGenerator implements PolygonGenerator {
+
 	private final PolygonGenerator generator;
 	private final float[] vertexes;
 	private final float scaleX;
 	private final float scaleY;
 	private boolean isGenerated;
-	
+
 	/**
-	 * Return a new {@link PolygonGenerator} that is a scaled version of the argument
+	 * Return a new {@link PolygonGenerator} that is a scaled version of the
+	 * argument
+	 * 
 	 * @param generatorArg
-	 * @param scale the scale (same on X and Y axis)
+	 * @param scale
+	 *            the scale (same on X and Y axis)
 	 */
 	public PolygonScaler(PolygonGenerator generatorArg, float scale) {
 		this(generatorArg, scale, scale);
 	}
-	
+
 	/**
-	 * Return a new {@link PolygonGenerator} that is a scaled version of the argument
+	 * Return a new {@link PolygonGenerator} that is a scaled version of the
+	 * argument
+	 * 
 	 * @param generatorArg
-	 * @param scaleXArg the scale on X axis
-	 * @param scaleYArg the scale on Y axis
+	 * @param scaleXArg
+	 *            the scale on X axis
+	 * @param scaleYArg
+	 *            the scale on Y axis
 	 */
 	public PolygonScaler(PolygonGenerator generatorArg, float scaleXArg, float scaleYArg) {
 		generator = generatorArg;
@@ -43,13 +50,13 @@ public class PolygonScaler extends AbstractPolygonGenerator implements
 
 	@Override
 	protected float[] generate() {
-		if(isGenerated) {
+		if (isGenerated) {
 			return vertexes;
 		}
-		
-		for(int i = 0 ; i < vertexes.length /2 ; i++) {
-			vertexes[2*i] *= scaleX;
-			vertexes[2*i + 1] *= scaleY;
+
+		for (int i = 0; i < vertexes.length / 2; i++) {
+			vertexes[2 * i] *= scaleX;
+			vertexes[2 * i + 1] *= scaleY;
 		}
 
 		isGenerated = true;
