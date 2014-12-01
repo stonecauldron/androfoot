7b9e44157d61faec2fa5220f24661dfa9c37c818
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-12-01 13:37:52 +0100

    add a test to detect bug efd0ec93187c1933c11b38461e2dfc503cfb921b

--- core/test/ch/epfl/sweng/androfoot/polygongenerator/test/CircleGeneratorTest.java
+++ core/test/ch/epfl/sweng/androfoot/polygongenerator/test/CircleGeneratorTest.java
@@ -18,106 +18,146 @@ public class CircleGeneratorTest {
 
 	@Test
 	public void testAngleConverter() {
-		assertEquals("The converter failed", Math.PI, PolygonUtils.degreeToRadian(180f), DELTA);
-		assertEquals("The converter failed", 0d, PolygonUtils.degreeToRadian(0f), DELTA);
-		assertEquals("The converter failed", Math.PI/2, PolygonUtils.degreeToRadian(90f), DELTA);
-		assertEquals("The converter failed", 4*Math.PI, PolygonUtils.degreeToRadian(720f), DELTA);
+		assertEquals("The converter failed", Math.PI,
+				PolygonUtils.degreeToRadian(180f), DELTA);
+		assertEquals("The converter failed", 0d,
+				PolygonUtils.degreeToRadian(0f), DELTA);
+		assertEquals("The converter failed", Math.PI / 2,
+				PolygonUtils.degreeToRadian(90f), DELTA);
+		assertEquals("The converter failed", 4 * Math.PI,
+				PolygonUtils.degreeToRadian(720f), DELTA);
 	}
-	
+
 	@Test
 	public void testAngleSimplifier() {
-		assertEquals("The simplifier failed", 0d, PolygonUtils.simplifyRadianAngle((float) (Math.PI*8)), DELTA);
-		assertEquals("The simplifier failed", 0d, PolygonUtils.simplifyRadianAngle((float) (Math.PI*4)), DELTA);
-		assertEquals("The simplifier failed", Math.PI, PolygonUtils.simplifyRadianAngle((float) (Math.PI*5)), DELTA);
-		assertEquals("The simplifier failed", Math.PI*3d/2d, PolygonUtils.simplifyRadianAngle((float) (-Math.PI/2)), DELTA);
+		assertEquals("The simplifier failed", 0d,
+				PolygonUtils.simplifyRadianAngle((float) (Math.PI * 8)), DELTA);
+		assertEquals("The simplifier failed", 0d,
+				PolygonUtils.simplifyRadianAngle((float) (Math.PI * 4)), DELTA);
+		assertEquals("The simplifier failed", Math.PI,
+				PolygonUtils.simplifyRadianAngle((float) (Math.PI * 5)), DELTA);
+		assertEquals("The simplifier failed", Math.PI * 3d / 2d,
+				PolygonUtils.simplifyRadianAngle((float) (-Math.PI / 2)), DELTA);
 	}
-	
+
 	@Test
 	public void testRoundCircle() {
 		CircleGenerator generator = new CircleGenerator(SEGMENTS);
 		float[] vertexes = generator.generateVertexesFloat();
-		assertEquals("The amount of vertex is false", SEGMENTS + 2, (vertexes.length) / 2);
-		assertEquals("The last vertex is not the same as the first one", vertexes[0], vertexes[SEGMENTS*2], DELTA);
-		assertEquals("The last vertex is not the same as the first one", vertexes[1], vertexes[SEGMENTS*2 + 1], DELTA);
+		assertEquals("The amount of vertex is false", SEGMENTS + 2,
+				(vertexes.length) / 2);
+		assertEquals("The last vertex is not the same as the first one",
+				vertexes[0], vertexes[SEGMENTS * 2], DELTA);
+		assertEquals("The last vertex is not the same as the first one",
+				vertexes[1], vertexes[SEGMENTS * 2 + 1], DELTA);
 	}
-	
-	@Test 
+
+	@Test
 	public void testEquivalentRepresentations() {
 		Random rnd = new Random();
-		CircleGenerator generator = new CircleGenerator(SEGMENTS, 
-				rnd.nextFloat(), rnd.nextFloat(), AngleType.RADIAN, rnd.nextFloat());
-		
+		CircleGenerator generator = new CircleGenerator(SEGMENTS,
+				rnd.nextFloat(), rnd.nextFloat(), AngleType.RADIAN,
+				rnd.nextFloat());
+
 		float zPlane = rnd.nextFloat();
-		
+
 		float[] vertexes = generator.generateVertexesFloat();
 		float[] vertexes3D = generator.generateVertexesFloatInZPlane(zPlane);
 		List<ImmutablePoint<Float>> points = generator.generatePointsList();
-		
-		for(int i = 0 ; i <= SEGMENTS ; i++) {
-			assertEquals("xVertex and xVertex3D different", vertexes[2*i], vertexes3D[3*i], DELTA);
-			assertEquals("yVertex and yVertex3D different", vertexes[2*i + 1], vertexes3D[3*i + 1], DELTA);
-			assertEquals("vertex3D not in Zplane", zPlane, vertexes3D[3*i + 2], DELTA);
-			assertEquals("xVertex and xListPoint different", vertexes[2*i], points.get(i).x, DELTA);
-			assertEquals("yVertex and yListPoint different", vertexes[2*i + 1], points.get(i).y, DELTA);
+
+		for (int i = 0; i <= SEGMENTS; i++) {
+			assertEquals("xVertex and xVertex3D different", vertexes[2 * i],
+					vertexes3D[3 * i], DELTA);
+			assertEquals("yVertex and yVertex3D different",
+					vertexes[2 * i + 1], vertexes3D[3 * i + 1], DELTA);
+			assertEquals("vertex3D not in Zplane", zPlane,
+					vertexes3D[3 * i + 2], DELTA);
+			assertEquals("xVertex and xListPoint different", vertexes[2 * i],
+					points.get(i).x, DELTA);
+			assertEquals("yVertex and yListPoint different",
+					vertexes[2 * i + 1], points.get(i).y, DELTA);
 		}
 	}
-	
+
 	@Test
 	public void testPointsArroundCircle() {
 		Random rnd = new Random();
 		float radius = Math.abs(rnd.nextFloat() + rnd.nextInt(5));
-		CircleGenerator generator = new CircleGenerator(SEGMENTS, 
+		CircleGenerator generator = new CircleGenerator(SEGMENTS,
 				rnd.nextFloat(), rnd.nextFloat(), AngleType.RADIAN, radius);
-		
-		for(ImmutablePoint<Float> p : generator.generatePointsList()) {
-			assertEquals("the point is not on the circle", radius, Math.sqrt(p.x*p.x + p.y*p.y), DELTA);
+
+		for (ImmutablePoint<Float> p : generator.generatePointsList()) {
+			assertEquals("the point is not on the circle", radius,
+					Math.sqrt(p.x * p.x + p.y * p.y), DELTA);
 		}
 	}
-	
+
+	@Test
+	public void testPointNotStacked() {
+		CircleGenerator generator = new CircleGenerator(SEGMENTS * 10);
+		List<ImmutablePoint<Float>> points = generator.generatePointsList();
+		points.remove(0);
+
+		for (ImmutablePoint<Float> referencePoint : points) {
+			int nbSame = 0;
+			for (ImmutablePoint<Float> comparePoint : points) {
+				if (referencePoint == comparePoint) {
+					nbSame++;
+				}
+			}
+			assertEquals("All points should be differents", 1, nbSame);
+		}
+	}
+
 	@Test
 	public void testEquivalentPortions() {
 		Random rnd = new Random();
 		float from = rnd.nextFloat();
 		float to = rnd.nextFloat();
-		CircleGenerator generator = new CircleGenerator(SEGMENTS, from, to, AngleType.RADIAN);
-		CircleGenerator generator2 = new CircleGenerator(SEGMENTS, to, from, AngleType.RADIAN);
-		
+		CircleGenerator generator = new CircleGenerator(SEGMENTS, from, to,
+				AngleType.RADIAN);
+		CircleGenerator generator2 = new CircleGenerator(SEGMENTS, to, from,
+				AngleType.RADIAN);
+
 		assertArrayEquals("the two arrays are different",
-				generator.generateVertexesFloat(), generator2.generateVertexesFloat(), (float)DELTA);
+				generator.generateVertexesFloat(),
+				generator2.generateVertexesFloat(), (float) DELTA);
 	}
-	
-	@Test 
+
+	@Test
 	public void testRadiansDegreeSame() {
 		Random rnd = new Random();
 		float from = rnd.nextFloat() + rnd.nextInt(360);
 		float to = rnd.nextFloat() + rnd.nextInt(360);
-		CircleGenerator generator = new CircleGenerator(SEGMENTS, 
-				from, to, AngleType.DEGREE);
+		CircleGenerator generator = new CircleGenerator(SEGMENTS, from, to,
+				AngleType.DEGREE);
 
-		CircleGenerator generator2 = new CircleGenerator(SEGMENTS, 
-				PolygonUtils.degreeToRadian(from), PolygonUtils.degreeToRadian(to),
-				AngleType.RADIAN);
+		CircleGenerator generator2 = new CircleGenerator(SEGMENTS,
+				PolygonUtils.degreeToRadian(from),
+				PolygonUtils.degreeToRadian(to), AngleType.RADIAN);
 
-		assertArrayEquals("the two arrays are different", 
-				generator.generateVertexesFloat(), generator2.generateVertexesFloat(), (float)DELTA);
+		assertArrayEquals("the two arrays are different",
+				generator.generateVertexesFloat(),
+				generator2.generateVertexesFloat(), (float) DELTA);
 	}
-	
-	@Test 
+
+	@Test
 	public void testSimplifiedAngleSame() {
 		Random rnd = new Random();
 		float from = rnd.nextFloat() + rnd.nextInt(360);
 		float to = rnd.nextFloat() + rnd.nextInt(360);
 
-		CircleGenerator generator = new CircleGenerator(SEGMENTS, 
-				PolygonUtils.simplifyRadianAngle(from), PolygonUtils.simplifyRadianAngle(to),
-				AngleType.RADIAN);
+		CircleGenerator generator = new CircleGenerator(SEGMENTS,
+				PolygonUtils.simplifyRadianAngle(from),
+				PolygonUtils.simplifyRadianAngle(to), AngleType.RADIAN);
 
-		CircleGenerator generator2 = new CircleGenerator(SEGMENTS, 
-				PolygonUtils.simplifyRadianAngle(from), PolygonUtils.simplifyRadianAngle(to),
-				AngleType.RADIAN);
+		CircleGenerator generator2 = new CircleGenerator(SEGMENTS,
+				PolygonUtils.simplifyRadianAngle(from),
+				PolygonUtils.simplifyRadianAngle(to), AngleType.RADIAN);
 
-		assertArrayEquals("the two arrays are different", 
-				generator.generateVertexesFloat(), generator2.generateVertexesFloat(), (float)DELTA);
-		
+		assertArrayEquals("the two arrays are different",
+				generator.generateVertexesFloat(),
+				generator2.generateVertexesFloat(), (float) DELTA);
+
 	}
 }
