7e3266aac39b6ea4b06364d8985a6e4d5e2220ce
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2014-12-01 18:56:02 +0100

    Move network test class

--- core/test/ch/epfl/sweng/androfoot/kryonetnetworking/test/NetworkTest.java
+++ core/test/ch/epfl/sweng/androfoot/kryonetnetworking/test/NetworkTest.java
@@ -1,9 +1,12 @@
-package ch.epfl.sweng.androfoot.kryonetnetworking;
+package ch.epfl.sweng.androfoot.kryonetnetworking.test;
 
 import static org.junit.Assert.fail;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import ch.epfl.sweng.androfoot.kryonetnetworking.PlayerClient;
+import ch.epfl.sweng.androfoot.kryonetnetworking.PlayerHost;
 
 public class NetworkTest {
 
