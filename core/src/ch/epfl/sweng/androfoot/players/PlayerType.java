d7dc6195938d16cfc1e9df247da05672d64699db
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2014-12-03 16:32:05 +0100

    Fix Checkstyle in PlayerType enum

--- core/src/ch/epfl/sweng/androfoot/players/PlayerType.java
+++ core/src/ch/epfl/sweng/androfoot/players/PlayerType.java
@@ -1,5 +1,11 @@
 package ch.epfl.sweng.androfoot.players;
 
+/**
+ * Enum representing the classification of a given player.
+ * 
+ * @author Pedro Caldeira <pedrocaldeira>
+ *
+ */
 public enum PlayerType {
 	LOCAL_PLAYER, REMOTE_PLAYER, RANDOM_AI_PLAYER
 }
