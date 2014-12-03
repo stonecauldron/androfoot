a6099d137509ac064d8192e1b53dd31a9ebff250
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2014-12-03 16:29:58 +0100

    Fix Checkstyle in PlayerNumber enum

--- core/src/ch/epfl/sweng/androfoot/players/PlayerNumber.java
+++ core/src/ch/epfl/sweng/androfoot/players/PlayerNumber.java
@@ -1,5 +1,11 @@
 package ch.epfl.sweng.androfoot.players;
 
+/**
+ * Enum representing the team of a given player.
+ * 
+ * @author Pedro Caldeira <pedrocaldeira>
+ *
+ */
 public enum PlayerNumber {
 	ONE, TWO, MOCK
 }
