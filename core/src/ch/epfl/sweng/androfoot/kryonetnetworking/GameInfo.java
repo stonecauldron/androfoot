aad6b337975600fe782b16152c06be9646d8e311
Adan Häfliger <adan.haefliger@epfl.ch>
2014-12-05 13:34:05 +0100

    create Game info data class

---
+++ core/src/ch/epfl/sweng/androfoot/kryonetnetworking/GameInfo.java
@@ -0,0 +1,12 @@
+package ch.epfl.sweng.androfoot.kryonetnetworking;
+
+/**
+ * @author Ahaeflig
+ * 
+ *         Class used to abstract general game settings that player must send to
+ *         each other to have synchronous board
+ */
+public class GameInfo {
+
+	
+}
