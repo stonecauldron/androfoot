1861d2e97513d718fc241992403632144ac74c97
Adan Häfliger <adan.haefliger@epfl.ch>
2014-12-05 18:21:26 +0100

    Rework network interface

--- core/src/ch/epfl/sweng/androfoot/interfaces/HostObservable.java
+++ core/src/ch/epfl/sweng/androfoot/interfaces/HostObservable.java
@@ -1,10 +1,15 @@
 package ch.epfl.sweng.androfoot.interfaces;
 
+import ch.epfl.sweng.androfoot.kryonetnetworking.GameInfo;
 import ch.epfl.sweng.androfoot.kryonetnetworking.InputData;
+import ch.epfl.sweng.androfoot.kryonetnetworking.ShakeData;
 
 public interface HostObservable {
 
 	public void addHostObserver(HostObserver obs);
 	boolean removeHostObserver(HostObserver obs);
 	public void updateHostObserver(InputData data);
+	public void updateHostObserver(ShakeData data);
+	public void updateHostObserver(GameInfo data);
+	
 }
