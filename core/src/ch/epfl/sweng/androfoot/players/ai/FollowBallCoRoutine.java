7398359bfe1de7582d2deefc047860858bdecfef
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2014-12-07 15:39:53 +0100

    Create followBallCoRoutine

---
+++ core/src/ch/epfl/sweng/androfoot/players/ai/FollowBallCoRoutine.java
@@ -0,0 +1,52 @@
+package ch.epfl.sweng.androfoot.players.ai;
+
+import java.util.Arrays;
+import java.util.List;
+
+import ch.epfl.sweng.androfoot.utils.CoRoutine;
+
+/**
+ * Class to represent the follow ball behaviour.
+ * 
+ * @author Pedro Caldeira <pedrocaldeira>
+ *
+ */
+public class FollowBallCoRoutine implements CoRoutine {
+
+	private static final int MAX_DELTA_Y = 10;
+	private static final float TOLERANCE = 0.01f;
+
+	private AbstractAIPlayer mPaddles;
+
+	private List<AIState> authorizedStates = Arrays.asList(AIState.DEFENSE);
+
+	@Override
+	public void execute() {
+		followBall();
+	}
+
+	@Override
+	public List<AIState> getStatesWhereCoRoutineIsExecutable() {
+		return authorizedStates;
+	}
+
+	private void followBall() {
+		float playerY = mPaddles.GetYPositionOfPlayerThatCanReachTheBall();
+		float ballY = mPaddles.GetYPositionOfPlayerThatCanReachTheBall();
+
+		// player is in front of ball
+		if (Math.abs(ballY - playerY) < TOLERANCE) {
+			// do not move
+			mPaddles.move(0, 0);
+		} else {
+			if (playerY > ballY) {
+				// go down
+				mPaddles.moveVertically(-MAX_DELTA_Y);
+			} else if (playerY < ballY) {
+				// go up
+				mPaddles.moveVertically(MAX_DELTA_Y);
+			}
+		}
+	}
+
+}
