38ef940389636db9010c3e2fa202bf9860b0cfd3
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2014-12-07 18:24:09 +0100

    Add player types for hard, medium and easy AI

--- core/src/ch/epfl/sweng/androfoot/players/PlayerType.java
+++ core/src/ch/epfl/sweng/androfoot/players/PlayerType.java
@@ -7,5 +7,5 @@ package ch.epfl.sweng.androfoot.players;
  *
  */
 public enum PlayerType {
-	LOCAL_PLAYER, REMOTE_PLAYER, RANDOM_AI_PLAYER
+	LOCAL_PLAYER, REMOTE_PLAYER, RANDOM_AI_PLAYER, EASY_AI_PLAYER, MEDIUM_AI_PLAYER, HARD_AI_PLAYER
 }
