e6e9a77c64c7473b71968b7f516bc68dc5a4d4a5
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2014-12-07 18:25:39 +0100

    Create easy AI class

---
+++ core/src/ch/epfl/sweng/androfoot/players/ai/EasyAI.java
@@ -0,0 +1,17 @@
+package ch.epfl.sweng.androfoot.players.ai;
+
+import ch.epfl.sweng.androfoot.players.PlayerNumber;
+import ch.epfl.sweng.androfoot.utils.Timer;
+
+public class EasyAI extends AbstractAIPlayer {
+
+	private static final float FOLLOW_BALL_TIMER = 0.1f;
+	private static final float DISTANCE_FACTOR = 5f;
+
+	public EasyAI(PlayerNumber number) {
+		super(number);
+
+		addToCoRoutines(new Timer(FOLLOW_BALL_TIMER), new FollowBallCoRoutine(
+				this, DISTANCE_FACTOR));
+	}
+}
