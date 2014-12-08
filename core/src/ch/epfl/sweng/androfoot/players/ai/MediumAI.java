32b3681cdff998191e8845d70260c59d4174b284
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2014-12-08 19:42:47 +0100

    Medium AI has constants for timer ticks.

--- core/src/ch/epfl/sweng/androfoot/players/ai/MediumAI.java
+++ core/src/ch/epfl/sweng/androfoot/players/ai/MediumAI.java
@@ -7,13 +7,15 @@ public class MediumAI extends AbstractAIPlayer {
 
 	private static final float FOLLOW_BALL_TIMER = 0.001f;
 	private static final float DISTANCE_FACTOR = 2f;
+	private static final float SHOOT_TIMER = 0.25f;
+	private static final float RETREAT_TIMER = 0.1f;
 
 	public MediumAI(PlayerNumber number) {
 		super(number);
 
 		addToCoRoutines(new Timer(FOLLOW_BALL_TIMER), new FollowBallCoRoutine(
 				this, DISTANCE_FACTOR));
-		addToCoRoutines(new Timer(0.5f), new ShootBallCoRoutine(this));
-		addToCoRoutines(new Timer(0.1f), new RetreatCoRoutine(this));
+		addToCoRoutines(new Timer(SHOOT_TIMER), new ShootBallCoRoutine(this));
+		addToCoRoutines(new Timer(RETREAT_TIMER), new RetreatCoRoutine(this));
 	}
 }
