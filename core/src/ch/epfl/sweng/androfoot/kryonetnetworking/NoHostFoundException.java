a8dc241e2967869cb28e47593730385515f3d433
Adan Häfliger <adan.haefliger@epfl.ch>
2014-12-08 14:51:42 +0100

    Add exception when no host found

---
+++ core/src/ch/epfl/sweng/androfoot/kryonetnetworking/NoHostFoundException.java
@@ -0,0 +1,10 @@
+package ch.epfl.sweng.androfoot.kryonetnetworking;
+
+public class NoHostFoundException extends Exception {
+
+	/**
+	 *  
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
