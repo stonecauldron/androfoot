57a3b4372483153fe38537692a4517fed8adc130
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2014-12-15 15:04:41 +0100

    doc and checktyle for gamelogic package

--- core/src/ch/epfl/sweng/androfoot/gamelogic/PlayerCharacteristicsManager.java
+++ core/src/ch/epfl/sweng/androfoot/gamelogic/PlayerCharacteristicsManager.java
@@ -16,21 +16,18 @@ import ch.epfl.sweng.androfoot.rendering.GraphicEngine;
  */
 public class PlayerCharacteristicsManager {
 
-	private static float DEFAULT_PADDLE_HEIGHT_CIRCLE = 0.2f;
-	private static float DEFAULT_PADDLE_HEIGHT_RECTANGLE = 0.08f;
-	private static float DEFAULT_PADDLE_WIDTH = 0.8f;
+	private static final float DEFAULT_PADDLE_HEIGHT_CIRCLE = 0.2f;
+	private static final float DEFAULT_PADDLE_HEIGHT_RECTANGLE = 0.08f;
+	private static final float DEFAULT_PADDLE_WIDTH = 0.8f;
 
 	private static PaddleGenerator paddleGeneratorInstanceTeam1 = null;
 	private static PaddleGenerator paddleGeneratorInstanceTeam2 = null;
 
-	public static final Color[] TEAM_COLORS_HEX = new Color[]{	new Color(0xFF2020FF),
-																new Color(0x4040FFFF),
-																new Color(0x40E080FF),
-																new Color(0xC040FFFF),
-																new Color(0x00FFFFFF),
-																new Color(0xFF8040FF),
-																new Color(0xE0E040FF),
-																new Color(0xFFFFFFFF)};
+	public static final Color[] TEAM_COLORS_HEX = new Color[] {
+			new Color(0xFF2020FF), new Color(0x4040FFFF),
+			new Color(0x40E080FF), new Color(0xC040FFFF),
+			new Color(0x00FFFFFF), new Color(0xFF8040FF),
+			new Color(0xE0E040FF), new Color(0xFFFFFFFF) };
 
 	public static Color getColorTeam1() {
 		return TEAM_COLORS_HEX[Configuration.getInstance().getPlayerOneTeam()];
@@ -48,8 +45,8 @@ public class PlayerCharacteristicsManager {
 	public static PaddleGenerator getInstanceTeam1() {
 		if (paddleGeneratorInstanceTeam1 == null) {
 			paddleGeneratorInstanceTeam1 = new PaddleGenerator(
-					DEFAULT_PADDLE_WIDTH, DEFAULT_PADDLE_HEIGHT_CIRCLE,
-					DEFAULT_PADDLE_HEIGHT_RECTANGLE);
+							DEFAULT_PADDLE_WIDTH, DEFAULT_PADDLE_HEIGHT_CIRCLE,
+							DEFAULT_PADDLE_HEIGHT_RECTANGLE);
 		}
 		return paddleGeneratorInstanceTeam1;
 	}
@@ -62,16 +59,16 @@ public class PlayerCharacteristicsManager {
 	public static PaddleGenerator getInstanceTeam2() {
 		if (paddleGeneratorInstanceTeam2 == null) {
 			paddleGeneratorInstanceTeam2 = new PaddleGenerator(
-					DEFAULT_PADDLE_WIDTH, DEFAULT_PADDLE_HEIGHT_CIRCLE,
-					DEFAULT_PADDLE_HEIGHT_RECTANGLE);
+							DEFAULT_PADDLE_WIDTH, DEFAULT_PADDLE_HEIGHT_CIRCLE,
+							DEFAULT_PADDLE_HEIGHT_RECTANGLE);
 		}
 		return paddleGeneratorInstanceTeam2;
 	}
 
 	public static void setShapeParamsPlayer1(float heightShootZone,
-			float heightControlZone, float width) {
+					float heightControlZone, float width) {
 		paddleGeneratorInstanceTeam1 = new PaddleGenerator(width,
-				heightShootZone, heightControlZone);
+						heightShootZone, heightControlZone);
 		warnShapeHasChanged();
 	}
 
@@ -81,9 +78,9 @@ public class PlayerCharacteristicsManager {
 	}
 
 	public static void setShapeParamsPlayer2(float heightShootZone,
-			float heightControlZone, float width) {
+					float heightControlZone, float width) {
 		paddleGeneratorInstanceTeam2 = new PaddleGenerator(width,
-				heightShootZone, heightControlZone);
+						heightShootZone, heightControlZone);
 		warnShapeHasChanged();
 	}
 
--- core/src/ch/epfl/sweng/androfoot/gamelogic/powerups/BallSizePowerUp.java
+++ core/src/ch/epfl/sweng/androfoot/gamelogic/powerups/BallSizePowerUp.java
@@ -4,20 +4,28 @@ import ch.epfl.sweng.androfoot.box2dphysics.Constants;
 import ch.epfl.sweng.androfoot.box2dphysics.PhysicsWorld;
 import ch.epfl.sweng.androfoot.interfaces.PowerUpEffect;
 
+/**
+ * Represent a powerup which change the size of the ball
+ * 
+ * @author Guillaume
+ *
+ */
 public class BallSizePowerUp implements PowerUpEffect {
-	
+
 	private final static float DURATION = 30f;
 	private final static float RADIUS_FACTOR_REDUCTION = 0.5f;
 	private final static float RADIUS_FACTOR_AUGMENTATION = 2f;
-	private final static float NEW_SMALL_RADIUS = Constants.BALL_RADIUS*RADIUS_FACTOR_REDUCTION;
-	private final static float NEW_BIG_RADIUS = Constants.BALL_RADIUS* RADIUS_FACTOR_AUGMENTATION;
-	
+	private final static float NEW_SMALL_RADIUS = Constants.BALL_RADIUS
+					* RADIUS_FACTOR_REDUCTION;
+	private final static float NEW_BIG_RADIUS = Constants.BALL_RADIUS
+					* RADIUS_FACTOR_AUGMENTATION;
+
 	private final boolean isBig;
-	
+
 	BallSizePowerUp(boolean bigArg) {
 		isBig = bigArg;
 	}
-	
+
 	@Override
 	public float getEffectDuration() {
 		return DURATION;
@@ -25,16 +33,19 @@ public class BallSizePowerUp implements PowerUpEffect {
 
 	@Override
 	public void begin(boolean isTeam1) {
-		if(isBig) {
-			PhysicsWorld.getPhysicsWorld().getBall().changeFixture(NEW_BIG_RADIUS);
+		if (isBig) {
+			PhysicsWorld.getPhysicsWorld().getBall()
+							.changeFixture(NEW_BIG_RADIUS);
 		} else {
-			PhysicsWorld.getPhysicsWorld().getBall().changeFixture(NEW_SMALL_RADIUS);
+			PhysicsWorld.getPhysicsWorld().getBall()
+							.changeFixture(NEW_SMALL_RADIUS);
 		}
 	}
 
 	@Override
 	public void end() {
-		PhysicsWorld.getPhysicsWorld().getBall().changeFixture(Constants.BALL_RADIUS);
+		PhysicsWorld.getPhysicsWorld().getBall()
+						.changeFixture(Constants.BALL_RADIUS);
 	}
 
 	@Override
